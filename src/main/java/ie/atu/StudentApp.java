4ea20f994bfca285a1b5f37f2df858f501484fba
Jamie <G00419525@atu.ie>
2023-10-31 14:56:12 +0000

    Created a new java file called StudentApp for setters and getters

---
+++ src/main/java/ie/atu/StudentApp.java
@@ -0,0 +1,32 @@
+package ie.atu;
+
+public class StudentApp {
+    private String name;
+    private String email;
+    private String course;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
